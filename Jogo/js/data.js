1fe5593249e2b361fab7aafe13aa7aa164bfb17e
Matheus Sena <MathSena07@hotmail.com>
2019-01-18 04:58:51 -0200

    Adicionando Projeto

---
+++ Jogo/js/data.js
@@ -0,0 +1,83 @@
+const cardData = [
+  {
+    name: "img1",
+    image: "img1.png",
+    id: "img1"
+  },
+  {
+    name: "img2",
+    image: "img2.png",
+    id: "img2"
+  },
+  {
+    name: "img3",
+    image: "img3.png",
+    id: "img3"
+  },
+  {
+    name: "img4",
+    image: "img4.png",
+    id: "img4"
+  },
+  {
+    name: "img5",
+    image: "img5.png",
+    id: "img5"
+  },
+  {
+    name: "img6",
+    image: "img6.png",
+    id: "img6"
+  },
+  {
+    name: "img7",
+    image: "img7.png",
+    id: "img7"
+  },
+  {
+    name: "img8",
+    image: "img8.png",
+    id: "img8"
+  },
+  {
+    name: "img9",
+    image: "img9.png",
+    id: "img9"
+  },
+  {
+    name: "img10",
+    image: "img10.png",
+    id: "img10"
+  },
+  {
+    name: "img11",
+    image: "img11.png",
+    id: "img11"
+  },
+ {
+    name: "img12",
+    image: "img12.png",
+    id: "img12"
+  },
+];
+
+const gameLevels = {
+  easy: {
+    class: "easy",
+    pairs: 6,
+    twoStar: 6,
+    oneStar: 10
+  },
+  medium: {
+    class: "medium",
+    pairs: 8,
+    twoStar: 10,
+    oneStar: 14
+  },
+  hard: {
+    class: "hard",
+    pairs: 12,
+    twoStar: 16,
+    oneStar: 24
+  }
+};
